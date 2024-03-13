4e71eed81d61d369030f20b50df13c2123e624d4
woorykim <wor0927@gmail.com>
2024-03-14 04:24:37 +0900

    커스텀 훅 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useScroll } from "./hook/useScroll";
+import { useWindowResize } from "./hook/useWindowResize";
+import { HeadBar } from "./component/layout/HeadBar";
 
 function App() {
-  return <div>test</div>;
+  const { y } = useScroll();
+
+  useWindowResize();
+
+  return (
+    <div
+      style={{
+        backgroundColor: y > 100 ? "" : "auto",
+      }}
+    >
+      <HeadBar></HeadBar>
+      test
+    </div>
+  );
 }
 
 export default App;

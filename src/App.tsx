41ad8d93d2d19c176e362488d7eb744d7303be26
woorykim <wor0927@gmail.com>
2024-03-14 22:48:14 +0900

    스크롤 훅 추가 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
+import styled, { css } from "styled-components";
+
 import { useScroll } from "./hook/useScroll";
 import { useWindowResize } from "./hook/useWindowResize";
 import { HeadBar } from "./component/layout/HeadBar";
+import { Body } from "./component/layout/Body";
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 
 function App() {
   const { y } = useScroll();
@@ -9,14 +17,10 @@ function App() {
   useWindowResize();
 
   return (
-    <div
-      style={{
-        backgroundColor: y > 100 ? "" : "auto",
-      }}
-    >
-      <HeadBar></HeadBar>
-      test
-    </div>
+    <Wrapper>
+      <HeadBar scroll={y}></HeadBar>
+      <Body></Body>
+    </Wrapper>
   );
 }
 
